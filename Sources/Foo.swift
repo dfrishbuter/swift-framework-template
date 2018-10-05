15cc979ab6043ff781c07bec52c25e8c0af0202d
Dmitry Frishbuter <dfrishbuter@ronasit.com>
2018-10-05 16:34:37 +0600

    Create untemplated project structure

---
+++ Sources/Foo.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Dmitry Frishbuter on 05/10/2018.
+//  Copyright © 2018 OrganizationName. All rights reserved.
+//
+
+import Foundation
+
+open class Foo {
+
+    public init() {
+
+    }
+}
