ad545625caab2add0156b721a847c23e91ae9932
Dmitry Frishbuter <dfrishbuter@ronasit.com>
2018-10-05 18:27:44 +0600

    Implement template

--- Sources/Foo.swift
+++ Sources/Foo.swift
@@ -1,6 +1,5 @@
 //
-//  Created by Dmitry Frishbuter on 05/10/2018.
-//  Copyright © 2018 OrganizationName. All rights reserved.
+//  Copyright © {% now 'local', '%Y' %} {{ cookiecutter.organization_name }}. All rights reserved.
 //
 
 import Foundation
